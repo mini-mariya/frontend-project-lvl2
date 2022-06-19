70f5fe582c83a922431f2cf112ead307b78327a5
mini_mariya <m3305@mail.ru>
2022-06-19 19:12:30 +0300

    add gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
-  .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'display help for command')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
