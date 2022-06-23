68a0942c1a36415646619b6d28b92dd65d25f844
mini_mariya <m3305@mail.ru>
2022-06-23 21:40:35 +0300

    add src/index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,46 @@
+import fs from 'fs';
+import path from 'path';
 import _ from 'lodash';
 
-const genDiff = (data1, data2) => {
-  let result = {};
+const getPathFile = (filepath) => path.resolve(process.cwd(), filepath);
 
-  const keys1 = _.keys(data1);
-  const keys2 = _.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+const getFileContent = (filepath) => {
+  const fileContent = fs.readFileSync(getPathFile(filepath), 'utf8');
+  return JSON.parse(fileContent);
+};
 
-  result = keys.map((key) => {
-    if (!_.has(data1, key)) {
-      return {
-        name: key,
-        value: data2[key],
-        type: 'added',
-      };
-    }
-    if (!_.has(data2, key)) {
-      return {
-        name: key,
-        value: data1[key],
-        type: 'deleted',
-      };
-    }
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return {
-        name: key,
-        type: 'nested',
-        children: genDiff(data1[key], data2[key]),
-      };
-    }
-    if (data1[key] !== data2[key]) {
-      return {
-        name: key,
-        value1: data1[key],
-        value2: data2[key],
-        type: 'changed',
-      };
-    }
-    return {
-      name: key,
-      value: data1[key],
-      type: 'unchanged',
-    };
-  });
-  return result;
+const compareFileContent = (obj1, obj2, replacer = ' ', spaceCount = 2) => {
+  const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
+  const currentSpace = replacer.repeat(spaceCount);
+  const lines = _.sortBy(uniqKeys)
+    .map((key) => {
+      const key1 = _.has(obj1, key);
+      const key2 = _.has(obj2, key);
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+      if (!key1) {
+        return [`+ ${key}: ${value2}`];
+      }
+      if (!key2) {
+        return [`- ${key}: ${value1}`];
+      }
+      if (_.isEqual(value1, value2)) {
+        return [`  ${key}: ${value1}`];
+      }
+      if (!_.isEqual(value1, value2)) {
+        return [`- ${key}: ${value1}`, `+ ${key}: ${value2}`];
+      }
+      throw new Error(`Unknown order state: '${key}'!`);
+    })
+    .flat();
+  const currentLines = lines.map((line) => `${currentSpace}${line}`);
+  return ['{', ...currentLines, '}'].join('\n');
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const file1 = getFileContent(filepath1);
+  const file2 = getFileContent(filepath2);
+  return compareFileContent(file1, file2);
 };
 
 export default genDiff;
