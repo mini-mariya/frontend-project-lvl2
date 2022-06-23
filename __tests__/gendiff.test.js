88712ca050fd38f717bd9ade0a44864342c1893c
mini_mariya <m3305@mail.ru>
2022-06-23 22:36:58 +0300

    add comparison yml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,15 +3,20 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test('check genDiff', () => {
-  const plainData = readFile('plain.txt');
-  const expected = plainData.trim();
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(actual).toEqual(expected);
+test('compare json', () => {
+  const expectedJson = readFile('expected-json.txt');
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(expectedJson);
+});
+
+test('compare yml', () => {
+  const expectedYml = readFile('expected-yml.txt');
+  expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yaml')).toBe(expectedYml);
 });
