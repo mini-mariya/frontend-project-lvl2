c3bdc9471e4c80835e08181e9e14a1f87af5bec8
mini_mariya <m3305@mail.ru>
2022-07-03 17:08:02 +0300

    delete blank line

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const getFixturePath = (filename) => path.resolve(process.cwd(), filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-
   const data1 = parse(readFile(filepath1), getFormat(filepath1));
   const data2 = parse(readFile(filepath2), getFormat(filepath2));
 
