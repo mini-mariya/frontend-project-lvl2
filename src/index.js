83e5fae67fa1075af6078506083329061a3550a3
mini_mariya <m3305@mail.ru>
2022-07-03 17:02:50 +0300

    combination of abstractions

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import parse from './parsers.js';
 import buildTree from './buildTree.js';
 import formatOutput from './formatters/index.js';
 
-const getFormat = (filename) => filename.split('.')[1];
+const getFormat = (filename) => path.extname(filename).slice(1);
 
 const getFixturePath = (filename) => path.resolve(process.cwd(), filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const firstFileContent = readFile(filepath1);
-  const secondFileContent = readFile(filepath2);
 
-  const data1 = parse(firstFileContent, getFormat(filepath1));
-  const data2 = parse(secondFileContent, getFormat(filepath2));
+  const data1 = parse(readFile(filepath1), getFormat(filepath1));
+  const data2 = parse(readFile(filepath2), getFormat(filepath2));
 
   const internalTree = buildTree(data1, data2);
   const formattedDiff = formatOutput(internalTree, format);
