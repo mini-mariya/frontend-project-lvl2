40905fe082e717e8f7c34eaf3c13e3d651eff0b4
mini_mariya <m3305@mail.ru>
2022-07-03 17:07:17 +0300

    transfer JSON.stringify

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,5 @@
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
-import makeJson from './json.js';
 
 export default (data, format = 'stylish') => {
   switch (format) {
@@ -9,7 +8,7 @@ export default (data, format = 'stylish') => {
     case 'plain':
       return makePlain(data);
     case 'json':
-      return makeJson(data);
+      return JSON.stringify(data);
     default:
       throw new Error(`Invalid file format type: '${format}'!`);
   }
